888f102fe394a349e7c7c716cea0bf4418eb2fb5
gumanzoy <gumanzoy@gmail.com>
2024-12-25 07:06:11 +0500

    Update /etc/console-setup and /etc/default/keyboard

--- bookworm/rootcopy/etc/console-setup/cached_setup_font.sh
+++ bookworm/rootcopy/etc/console-setup/cached_setup_font.sh
@@ -1,11 +1,11 @@
 #!/bin/sh
 
-setfont '/etc/console-setup/cached_Uni2-TerminusBoldVGA16.psf.gz' 
+setfont '/usr/share/consolefonts/CyrKoi-VGA16.psf.gz' 
 
 if ls /dev/fb* >/dev/null 2>/dev/null; then
     for i in /dev/vcs[0-9]*; do
         { :
-            setfont '/etc/console-setup/cached_Uni2-TerminusBoldVGA16.psf.gz' 
+            setfont '/etc/console-setup/cached_CyrKoi-VGA16.psf.gz' 
         } < /dev/tty${i#/dev/vcs} > /dev/tty${i#/dev/vcs}
     done
 fi
