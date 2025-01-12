8d7e50857c48212ca673b964b7532101303c52c7
gumanzoy <gumanzoy@gmail.com>
2025-01-13 00:31:39 +0500

    Remove rootcopy/etc/acpi/ folder

--- bookworm/rootcopy/etc/acpi/events/lid.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/sh
-case "$3" in
-    close)
-# suspend to RAM
-echo -n mem > /sys/power/state
-### comment out previous line and uncomment the next to SHUTDOWN when closing lid
-# poweroff
-;;
-esac
