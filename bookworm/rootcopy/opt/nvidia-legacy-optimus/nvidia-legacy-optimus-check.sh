55f8c2c3e8e514542c7cb17f81baa9618fad038d
gumanzoy <gumanzoy@gmail.com>
2025-03-19 00:32:27 +0500

    Upd nvidia-legacy-optimus-check.sh and /etc/rc.local

--- bookworm/rootcopy/opt/nvidia-legacy-optimus/nvidia-legacy-optimus-check.sh
+++ bookworm/rootcopy/opt/nvidia-legacy-optimus/nvidia-legacy-optimus-check.sh
@@ -2,8 +2,7 @@
 
 if egrep -qe "nv340|nv390" /mnt/live/tmp/modules; then
   if grep -q i915 /proc/modules; then
-  echo -e "\E[32m\033[1mnvidia-legacy-optimus-enabled"
-  tput sgr0
+  echo -e "\033[1;32m""nvidia-legacy-optimus-enabled""\033[0m"
 
   cp -f /opt/nvidia-legacy-optimus/nvidia-legacy-optimus-outputclass.conf \
   /etc/X11/xorg.conf.d/ 2> /dev/null
